d0d9ff7021d4549e6b646e5d0907b8d22308f940
영욱 <yeonguk0201@gmail.com>
2024-06-05 20:30:07 +0900

    fix navbar

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(mongoSanitize());
 
 const secret = process.env.SECRET || 'thisissecret';
